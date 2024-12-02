c5c724a23c785b69c035aa3f899a5dc519754fb5
LuisGitBucket <1230196@isep.ipp.pt>
2024-12-02 21:48:12 +0000

    Implemented RBAC to restrict access to Producer and Admin pages.

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -17,6 +17,7 @@ import { supabase } from "@/lib/supabase";
 
 function Sidebar() {
   const router = useRouter();
+  const [userRole, setUserRole] = useState(null);
 
   const logout = async () => {
     await supabase.auth.signOut();
@@ -51,6 +52,21 @@ function Sidebar() {
     router.push("/admin");
   };
 
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      if (session) {
+        // Get the role from the registration metadata
+        const role = session.user.user_metadata.role;
+        setUserRole(role);
+      }
+    };
+  
+    fetchUserRole();
+  }, []);
+  
+
   return (
     <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
       <img className="m-1 h-13 w-20" src="/soc.png" alt="" />
@@ -58,10 +74,18 @@ function Sidebar() {
       <SidebarRow Icon={ShoppingCartIcon} title="Cart" onClick={goToCart} />
       <SidebarRow Icon={ClipboardIcon} title="Orders" onClick={goToOrders}/>
       <SidebarRow Icon={CreditCardIcon} title="Subsciptions" onClick={goToSubscriptions}/>
-      {/* TODO: Validation according to the value received from RBAC */}
-      <SidebarRow Icon={ShieldCheckIcon} title="Producer Page" onClick={goToProducer} />
-      {/* TODO: Validation according to the value received from RBAC */}
-      <SidebarRow Icon={AdjustmentsIcon} title="Admin Page" onClick={goToAdmin} />
+      {/* SidebarRow only visible to Producers and Admins */}
+      {
+        (userRole === "Producer" || userRole === "Admin") && (
+          <SidebarRow Icon={ShieldCheckIcon} title="Producer Page" onClick={goToProducer} />
+        )
+      }
+      {/* SidebarRow only visible to Admins */}
+      {
+        userRole === "Admin" && (
+          <SidebarRow Icon={AdjustmentsIcon} title="Admin Page" onClick={goToAdmin} />
+        )
+      }
       <SidebarRow Icon={UserIcon} title="Profile" onClick={goToProfile} />
       <SidebarRow Icon={LogoutIcon} title="Sign Out" onClick={logout} />
     </div>
