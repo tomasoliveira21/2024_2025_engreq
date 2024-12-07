0aac9910c790bc29af2956e2e6cabf1c55cdacb5
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-12-07 00:54:34 +0000

    feat: baskets list and baskets mapping

--- frontend/src/app/amap/[id]/page.tsx
+++ frontend/src/app/amap/[id]/page.tsx
@@ -9,6 +9,8 @@ import { supabase } from "@/lib/supabase";
 import { fetchProducts } from "@/api/fetchProducts";
 import { Product } from "@/types/product";
 import Table from "../../../../components/Table";
+import { fetchBaskets } from "@/api/fetchBaskets";
+import { Basket } from "@/types/basket";
 
 export default function Amap({ params }: { params: { id: string } }) {
   const router = useRouter();
@@ -16,6 +18,7 @@ export default function Amap({ params }: { params: { id: string } }) {
   const [session, setSession] = useState<Session | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState<Product[]>([]);
+  const [baskets, setBaskets] = useState<Basket[]>([]);
 
   useEffect(() => {
     async function getSession() {
@@ -34,6 +37,24 @@ export default function Amap({ params }: { params: { id: string } }) {
           setIsLoading(true);
           const fetchedProducts = await fetchProducts(session.access_token, id);
           setProducts(fetchedProducts);
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    getProducts();
+  }, [session]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      if (session) {
+        try {
+          setIsLoading(true);
+          const fetchedBaskets = await fetchBaskets(session.access_token, id);
+          setBaskets(fetchedBaskets);
         } catch (error) {
           console.error("Error fetching products:", error);
         } finally {
@@ -90,6 +111,32 @@ export default function Amap({ params }: { params: { id: string } }) {
             />
           ) : (
             <div>No products available.</div>
+          )}
+
+          <h1 className="text-lg font-bold mb-2">Amap Baskets:</h1>
+          {baskets.length > 0 ? (
+            <Table
+              headers={["Basket Name", "Basket Type", "Basket Price"]}
+              data={baskets}
+              renderRow={(basket) => (
+                <tr key={basket.id}>
+                  <td
+                    className="border border-gray-300 px-4 py-2 text-blue-600 cursor-pointer hover:underline"
+                    onClick={() => router.push(`/basket/${basket.id}`)}
+                  >
+                    {basket.name}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {basket.type}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {basket.price} €
+                  </td>
+                </tr>
+              )}
+            />
+          ) : (
+            <div>No baskets available.</div>
           )}
         </div>
       </main>
---
+++ frontend/src/types/basket.ts
@@ -0,0 +1,9 @@
+export interface Basket {
+    id: number;
+    name: string;
+    description: string;
+    photoUrl: string | null;
+    price: number;
+    weight: number;
+  }
+  
