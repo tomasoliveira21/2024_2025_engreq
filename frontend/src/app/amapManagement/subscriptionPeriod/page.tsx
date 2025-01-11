3933e88bf33cce9c8ddeb4216af3030779d5dc09
tomasoliveira21 <tomasoliveira1018@gmail.com>
2025-01-11 18:16:20 +0000

    feat: seasons period fetch

--- frontend/src/app/amapManagement/subscriptionPeriod/page.tsx
+++ frontend/src/app/amapManagement/subscriptionPeriod/page.tsx
@@ -4,11 +4,12 @@ import React, { useEffect, useState } from "react";
 import { Session } from "@supabase/auth-helpers-nextjs";
 import { supabase } from "@/lib/supabase";
 import Sidebar from "../../../../components/Sidebar";
+import { fetchAmapSeasons } from "@/api/fetchAmapSeasons";
 
 
 export default function Orders() {
   const [session, setSession] = useState<Session | null>(null);
-  //const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getSession() {
@@ -20,13 +21,14 @@ export default function Orders() {
     getSession();
   }, []);
 
-/*
+
   useEffect(() => {
     const getProducerBalance = async () => {
       if (session) {
         try {
           setIsLoading(true);
-          const fetchedProducerBalance = await fetchProducerBalance(session.access_token);
+          const fetchedAmapSeasons = await fetchAmapSeasons(session.access_token);
+          console.log('fetchedAmapSeasons: ', fetchedAmapSeasons);
         } catch (error) {
           console.error("Error fetching Producer Balance:", error);
         } finally {
@@ -37,7 +39,7 @@ export default function Orders() {
 
     getProducerBalance();
   }, [session]);
-  */
+  
 
   if (!session) {
     return <div>Loading session...</div>;
@@ -48,7 +50,7 @@ export default function Orders() {
     return <div>Loading data...</div>;
   }
   */
- 
+
   return (
     <div className="lg:max-w-8xl mx-auto min-h-screen overflow-hidden text-white">
       <main className="grid grid-cols-12 gap-8">
